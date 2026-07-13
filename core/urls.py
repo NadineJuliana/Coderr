c31602a3f0c3c3ccf4086c687aba9fd3ba3e4474
NadineJuliana <nadine.juliana.b@web.de>
2026-07-14 01:56:03 +0200

    feat: implement basic app endpoints

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('auth_app.urls')),
+    path('api/', include('profile_app.urls')),
+    path('api/', include('offers_app.urls')),
+    path('api/', include('orders_app.urls')),
+    path('api/', include('reviews_app.urls')),
 ]
