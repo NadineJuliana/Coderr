bb5179f11f20ccf0de9365278b9ccd0d5936ef47
NadineJuliana <nadine.juliana.b@web.de>
2026-07-17 08:10:47 +0200

    chore: add basic enpoint structure on all urls.py files

--- core/urls.py
+++ core/urls.py
@@ -19,9 +19,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('auth_app.urls')),
-    path('api/', include('profile_app.urls')),
-    path('api/', include('offers_app.urls')),
-    path('api/', include('orders_app.urls')),
-    path('api/', include('reviews_app.urls')),
+    path('api/', include('auth_app.api.urls')),
+    path('api/', include('profile_app.api.urls')),
+    path('api/', include('offers_app.api.urls')),
+    path('api/', include('orders_app.api.urls')),
+    path('api/', include('reviews_app.api.urls')),
 ]
