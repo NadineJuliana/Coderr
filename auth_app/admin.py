c102d2695094ee2471ee6859196eb9cf586c457c
NadineJuliana <nadine.juliana.b@web.de>
2026-07-18 21:24:43 +0200

    feat(auth): add custom user admin integration

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from auth_app.forms import (
+    CustomUserChangeForm,
+    CustomUserCreationForm,
+)
+
+from auth_app.models import User
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+
+    list_display = (
+        "username",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
