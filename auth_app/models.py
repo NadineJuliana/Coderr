b3b82fafdcc27b92d46afab13c8a680a1276c914
NadineJuliana <nadine.juliana.b@web.de>
2026-07-18 23:47:32 +0200

    feat(auth): add user type to custom user model

--- auth_app/models.py
+++ auth_app/models.py
@@ -5,5 +5,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    class UserType(models.TextChoices):
+        CUSTOMER = "customer", "Customer"
+        BUSINESS = "business", "Business"
+
+    type = models.CharField(
+        max_length=20,
+        choices=UserType.choices,
+    )
+
     def __str__(self):
         return self.username
