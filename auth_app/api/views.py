670c25d3d01756552edac1a725b30a80a1d513d3
NadineJuliana <nadine.juliana.b@web.de>
2026-07-18 23:47:22 +0200

    feat(auth): implement user registration

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import GenericAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+from .serializers import RegistrationSerializer
 
 # Create your views here.
+
+
+class RegistrationView(GenericAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = RegistrationSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, _ = Token.objects.get_or_create(user=user)
+
+        response_data = {
+            "token": token.key,
+            "username": user.username,
+            "email": user.email,
+            "user_id": user.id,
+        }
+
+        return Response(
+            response_data,
+            status=status.HTTP_201_CREATED,
+        )
