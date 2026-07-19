c371f819650d21307b8c8c789a5cee94e344d904
NadineJuliana <nadine.juliana.b@web.de>
2026-07-19 22:14:02 +0200

    feat(orders): add order admin configuration

--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from orders_app.models import Order
 
 # Register your models here.
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "title",
+        "customer_user",
+        "business_user",
+        "offer_type",
+        "status",
+        "price",
+        "created_at",
+        "updated_at",
+    ]
+
+    list_filter = [
+        "status",
+        "offer_type",
+        "created_at",
+        "updated_at",
+    ]
+
+    search_fields = [
+        "title",
+        "customer_user__username",
+        "customer_user__email",
+        "business_user__username",
+        "business_user__email",
+    ]
+
+    readonly_fields = [
+        "created_at",
+        "updated_at",
+    ]
+
+    ordering = [
+        "-created_at",
+    ]
