017d84769f3290498ee132b0012012fdddccd3f5
NadineJuliana <nadine.juliana.b@web.de>
2026-07-19 22:52:25 +0200

    feat(reviews): implement review endpoints

--- reviews_app/api/urls.py
+++ reviews_app/api/urls.py
@@ -1,3 +1,19 @@
 from django.urls import include, path
+from .views import (
+    ReviewDetailView,
+    ReviewListCreateView,
+)
 
-urlpatterns = []
+
+urlpatterns = [
+    path(
+        "reviews/",
+        ReviewListCreateView.as_view(),
+        name="review-list",
+    ),
+    path(
+        "reviews/<int:pk>/",
+        ReviewDetailView.as_view(),
+        name="review-detail",
+    ),
+]
