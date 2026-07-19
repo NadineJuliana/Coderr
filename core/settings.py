a84c407279447642e6b119bafb960bec33334e93
NadineJuliana <nadine.juliana.b@web.de>
2026-07-19 19:24:53 +0200

    chore: add django-filter configuration

--- core/settings.py
+++ core/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    "django_filters",
     'auth_app',
     'profile_app',
     'offers_app',
@@ -150,6 +151,9 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
     ],
 }
 
