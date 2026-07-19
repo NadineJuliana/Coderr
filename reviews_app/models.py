f1e52f2d3a674b523d2fff651f0ae0f036a40aee
NadineJuliana <nadine.juliana.b@web.de>
2026-07-19 22:27:45 +0200

    feat(reviews): add reviews model

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator,
+)
 
 # Create your models here.
+
+
+class Review(models.Model):
+    business_user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="received_reviews",
+    )
+
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="written_reviews",
+    )
+
+    rating = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ],
+    )
+
+    description = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=[
+                    "business_user",
+                    "reviewer",
+                ],
+                name=(
+                    "unique_review_per_business_user"
+                ),
+            ),
+        ]
+
+    def __str__(self):
+        return (
+            f"{self.reviewer} - "
+            f"{self.business_user}: "
+            f"{self.rating}"
+        )
