7b0e3609fe19173d2c81a457d3c560bded56a18f
NadineJuliana <nadine.juliana.b@web.de>
2026-07-19 22:12:45 +0200

    feat(orders): add order endpoint urls

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,3 +1,30 @@
-from django.urls import include, path
+from django.urls import path
+from .views import (
+    CompletedOrderCountView,
+    OrderCountView,
+    OrderDetailView,
+    OrderListCreateView,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "orders/",
+        OrderListCreateView.as_view(),
+        name="order-list",
+    ),
+    path(
+        "orders/<int:pk>/",
+        OrderDetailView.as_view(),
+        name="order-detail",
+    ),
+    path(
+        "order-count/<int:business_user_id>/",
+        OrderCountView.as_view(),
+        name="order-count",
+    ),
+    path(
+        "completed-order-count/<int:business_user_id>/",
+        CompletedOrderCountView.as_view(),
+        name="completed-order-count",
+    ),
+]
