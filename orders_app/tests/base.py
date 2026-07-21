421d4661205c810de38316c717ca7445486db439
NadineJuliana <nadine.juliana.b@web.de>
2026-07-21 20:41:03 +0200

    test(orders): add shared base setup for order tests

---
+++ orders_app/tests/base.py
@@ -0,0 +1,131 @@
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+
+from offers_app.models import Offer, OfferDetail
+from orders_app.models import Order
+
+User = get_user_model()
+
+
+class OrdersTestBase(APITestCase):
+
+    def setUp(self):
+        self.customer_user = User.objects.create_user(
+            username="customer_user",
+            email="customer@mail.de",
+            password="examplePassword",
+            type="customer",
+        )
+
+        self.other_customer_user = User.objects.create_user(
+            username="other_customer_user",
+            email="other_customer@mail.de",
+            password="examplePassword",
+            type="customer",
+        )
+
+        self.business_user = User.objects.create_user(
+            username="business_user",
+            email="business@mail.de",
+            password="examplePassword",
+            type="business",
+        )
+
+        self.other_business_user = User.objects.create_user(
+            username="other_business_user",
+            email="other_business@mail.de",
+            password="examplePassword",
+            type="business",
+        )
+
+        self.admin_user = User.objects.create_user(
+            username="admin_user",
+            email="admin@mail.de",
+            password="examplePassword",
+            type="business",
+            is_staff=True,
+        )
+
+        self.customer_token = Token.objects.create(
+            user=self.customer_user,
+        )
+
+        self.other_customer_token = Token.objects.create(
+            user=self.other_customer_user,
+        )
+
+        self.business_token = Token.objects.create(
+            user=self.business_user,
+        )
+
+        self.other_business_token = Token.objects.create(
+            user=self.other_business_user,
+        )
+
+        self.admin_token = Token.objects.create(
+            user=self.admin_user,
+        )
+
+    def authenticate(self, token):
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + token.key,
+        )
+
+    def create_order(
+        self,
+        *,
+        customer_user=None,
+        business_user=None,
+        status_value=Order.OrderStatus.IN_PROGRESS,
+        title="Test Order",
+    ):
+        return Order.objects.create(
+            customer_user=customer_user or self.customer_user,
+            business_user=business_user or self.business_user,
+            title=title,
+            revisions=2,
+            delivery_time_in_days=7,
+            price=200,
+            features=["Test Feature"],
+            offer_type="standard",
+            status=status_value,
+        )
+
+
+class OrdersEndpointTestBase(OrdersTestBase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.offer = Offer.objects.create(
+            user=self.business_user,
+            title="Logo Design",
+            description="Professionelles Logo Design",
+        )
+
+        self.offer_detail = OfferDetail.objects.create(
+            offer=self.offer,
+            title="Basic Logo Design",
+            revisions=3,
+            delivery_time_in_days=5,
+            price=150,
+            features=[
+                "Logo Design",
+                "Visitenkarten",
+            ],
+            offer_type="basic",
+        )
+
+        self.order = Order.objects.create(
+            customer_user=self.customer_user,
+            business_user=self.business_user,
+            title=self.offer_detail.title,
+            revisions=self.offer_detail.revisions,
+            delivery_time_in_days=(
+                self.offer_detail.delivery_time_in_days
+            ),
+            price=self.offer_detail.price,
+            features=self.offer_detail.features,
+            offer_type=self.offer_detail.offer_type,
+        )
